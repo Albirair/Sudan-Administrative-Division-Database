95a043ad310b02b6f33d28527e80bf1ea3c368ec
Albirair <musabalbirair@gmail.com>
2021-08-11 15:37:09 +0200

    Create SQL transactions for table creation and population
    Create "create tables.sql" for which creates tables "state" & "locality" along with transactions "populate states.sql" & "populate localities.sql"

---
+++ README.md
@@ -0,0 +1,3 @@
+A set of SQL transactions intended to create and populate 2 tables representing **Sudan**'s administrative division as states composed of localities.
+ 
+The names of localities have been obtained from [Sudan Presidency website](https://www.presidency.gov.sd/page/About-States?fbclid=IwAR1ihjCbE8o7JFrUEIOSpEGNBy-H7Wx_2c3fEhjg5coycbDmhjOQm68NgLU), although effort has been made to retrieve the English names from "authoritative" sources, some had been translated by [Albirair](https://github.com/Albirair).
---
+++ populate states.sql
@@ -0,0 +1,38 @@
+/*
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+BEGIN;
+INSERT INTO state VALUES
+(1, 'Khartoum', 'الخرطوم'),
+(2, 'Northern', 'الشمالية'),
+(3, 'River Nile', 'نهر النيل'),
+(4, 'Gezira', 'الجزيرة'),
+(5, 'Sennar', 'سنار'),
+(6, 'White Nile', 'النيل الأبيض'),
+(7, 'Blue Nile', 'النيل الأزرق'),
+(8, 'Red Sea', 'البحر الأحمر'),
+(9, 'AlQadarif', 'القضارف'),
+(10, 'Kassala', 'كسلا'),
+(11, 'North Kordofan', 'شمال كردفان'),
+(12, 'South Kordofan', 'جنوب كردفان'),
+(13, 'West Kordofan', 'غرب كردفان'),
+(14, 'North Darfur', 'شمال دارفور'),
+(15, 'South Darfur', 'جنوب دارفور'),
+(16, 'East Darfur', 'شرق دارفور'),
+(17, 'West Darfur', 'غرب دارفور'),
+(18, 'Central Darfur', 'وسط دارفور');
+COMMIT;
